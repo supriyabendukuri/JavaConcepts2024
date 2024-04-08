3232b2113a5832a626f8cf948d069ca13153ad5d
supriya <supriya@ideaelan.com>
2024-04-08 11:17:32 +0530

    new class added

---
+++ src/SeleniumJava/StringConcept.java
@@ -0,0 +1,20 @@
+package SeleniumJava;
+
+public class StringConcept {
+	
+	public static void main(String[] args) {
+		String surName = "Bendukuri";
+		String name= "Supriya";
+		int age= 27;
+		String profession = "Software Engineer";
+		boolean married = true;
+		
+		if (surName.equalsIgnoreCase("bendukuri")) {
+			System.out.println("surname is valid");
+		}
+		else {
+			System.out.println("surname not valid");
+		}
+	}
+
+}
